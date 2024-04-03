fe2cd703fb4f1de11ab413d5f4b5bb5c731decbc
Rixki <hara.bekerja@gmail.com>
2024-04-03 17:34:12 +0700

    fix: add splash screen

--- android/app/src/main/java/com/template/MainActivity.kt
+++ android/app/src/main/java/com/template/MainActivity.kt
@@ -6,6 +6,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
 
@@ -23,6 +24,7 @@ class MainActivity : ReactActivity() {
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this);
     super.onCreate(null)
   }
 }
--- screens/Home.screen.jsx
+++ screens/Home.screen.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {ScrollView, View, Text, Button} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const HomeScreen = ({navigation}) => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
       <View>
